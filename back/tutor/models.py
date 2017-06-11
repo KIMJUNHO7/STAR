48db2f286505b4efc531c4d4b4cbe9a4bc8af4e9
novelview9 <novelview9@gmail.com>
2017-06-11 15:00:08 +0900

    Init tutor app

---
+++ back/tutor/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Major(models.Model):
+
+    name = models.CharField('학과명', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class University(models.Model):
+
+    name = models.CharField('대학명', max_length=100)
+    majors = models.ManyToManyField(Major)
+
+    def __str__(self):
+        return self.name
+
+
+class Tutor(models.Model):
+
+    name = models.CharField('튜터이름', max_length=100)
+    name = models.CharField('학번', max_length=100)
+    university = models.ForeignKey(University)
+    major = models.ForeignKey(Major)
+
+    def __str__(self):
+        return self.name
+
